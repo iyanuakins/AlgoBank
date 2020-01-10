0c02b6ef70780f4a2599e5d68e2a6ecbc5f59439
Iyanu Akins <sundax4u2@gmail.com>
2020-01-10 21:23:56 +0100

    Added transaction id to transaction class

--- AlgoBank/Transaction.cs
+++ AlgoBank/Transaction.cs
@@ -13,6 +13,8 @@ namespace AlgoBank
         private string _SourceAccountType;
         private string _DestinationAccount;
         private string _DestinationAccountType;
+        private string _TransactionID;
+        public static int TransactionCounter = 10260187;
 
         public Transaction(string __type, double __amount, string __SourceAccount, string __SourceAccountType, string __DestinationAccount, string __DestinationAccountType)
         {
@@ -22,6 +24,10 @@ namespace AlgoBank
             _SourceAccountType = __SourceAccountType;
             _DestinationAccount = __DestinationAccount;
             _DestinationAccountType = __DestinationAccount;
+
+            string TransactionIDPrefix = __type == "deposit" ? "DP" :
+                                                __type == "withdrawal" ? "WD" : "TR";
+            _TransactionID = $"{TransactionIDPrefix}-{++TransactionCounter}";
         }
 
         public DateTime DateCreated { get => DateCreated;}
@@ -31,5 +37,6 @@ namespace AlgoBank
         public string SourceAccountType { get => _SourceAccountType; }
         public string DestinationAccount { get => _DestinationAccount; }
         public string DestinationAccountType { get => _DestinationAccountType; }
+        public string TransactionID { get => _TransactionID; }
     }
 }
